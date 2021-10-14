be1b5b0c169235311d8a7dc36d2440a451290915
HIT-k544 <19B908049@stu.hit.edu.cn>
2021-10-14 15:02:16 +0800

    a little update

--- test.py
+++ test.py
@@ -37,16 +37,16 @@ parser.add_argument('--save_folder', default='det_results/', type=str,
 args = parser.parse_args()
 
 
-def plot_bbox_labels(img, bbox, label, cls_color, test_scale=0.4):
+def plot_bbox_labels(img, bbox, label, cls_color, text_scale=0.4):
     x1, y1, x2, y2 = bbox
     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
     t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
     # plot bbox
     cv2.rectangle(img, (x1, y1), (x2, y2), cls_color, 2)
     # plot title bbox
-    cv2.rectangle(img, (x1, y1-t_size[1]), (int(x1 + t_size[0] * test_scale), y1), cls_color, -1)
+    cv2.rectangle(img, (x1, y1-t_size[1]), (int(x1 + t_size[0] * text_scale), y1), cls_color, -1)
     # put the test on the title bbox
-    cv2.putText(img, label, (int(x1), int(y1 - 5)), 0, test_scale, (0, 0, 0), 1, lineType=cv2.LINE_AA)
+    cv2.putText(img, label, (int(x1), int(y1 - 5)), 0, text_scale, (0, 0, 0), 1, lineType=cv2.LINE_AA)
 
     return img
 
@@ -62,7 +62,7 @@ def visualize(img, bboxes, scores, cls_inds, vis_thresh, class_colors, class_nam
                 cls_id = int(cls_inds[i])
                 cls_color = class_colors[cls_id]
             mess = '%s: %.2f' % (class_names[cls_id], scores[i])
-            img = plot_bbox_labels(img, bbox, mess, cls_color, test_scale=ts)
+            img = plot_bbox_labels(img, bbox, mess, cls_color, text_scale=ts)
 
     return img
         
